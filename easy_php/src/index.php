627459a3e16915fd72342966297fdb723124e9af
Yongz <y0n9zh@gmail.com>
2023-11-09 16:17:19 +0800

    Change

--- easy_php/src/index.php
+++ easy_php/src/index.php
@@ -5,18 +5,12 @@ include '/flag_change';
 $a = $_SERVER['argv'][0];
 $b = $_SERVER['argv'][1];
 $c = $_SERVER['HTTP_CLIENT_IP'];
-echo $a."<hr/ >";
-echo md5($a)."<hr/ >";
-echo $b."<hr/ >";
-echo  md5($b)."<hr/ >";
-echo $c;
-
 
 if ($a == md5($b) && $a != $b && $a == md5($a)) {
     if (isset($c)) {
         echo $flag;
     } else {
-        echo '提供一个client-ip';
+        echo '请提供一个 client-ip ';
     }
 }
 ?>
